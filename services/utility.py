bb72a5d4f395328558459e29add5a08ba7cbf617
kerry.qiu <kerry.qiu@ef.com>
2017-05-04 16:34:03 +0800

    add ptest

--- services/utility.py
+++ services/utility.py
@@ -1,5 +1,17 @@
+import datetime
 HOSTNAME = "https://qa.englishtown.com"
 
 
 class StatusCode:
     Success = 200
+
+
+def get_further_date(day=0):
+    """
+    this function default return today's date, format like '2017-05-03'
+    :param day:  if day = 0 means today
+    :return: further date
+    """
+    now = datetime.datetime.now()
+    further = now + datetime.timedelta(days=day)
+    return further.strftime('%Y-%m-%d')
